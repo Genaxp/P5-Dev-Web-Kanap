cfb641aaf39feedf55385e3bb4f951de48873781
Gena <num.search200@gmail.com>
2022-11-28 15:59:07 +0100

    création page confirmation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,24 +1,33 @@
-const orderId = getOrderId()
-displayOrderWithId(orderId)
-clearCache()
+// const orderId = getOrderId()
+// displayOrderWithId()
+// clearCache()
 
-// recherches des id dans le local storage
+// // recherches des id dans le local storage
+// function getOrderId(){
+// const queryString = window.location.search
+// const urlParams = new URLSearchParams(queryString)
+// return urlParams.get("orderId")
+
+// }
+
+// //envoi les id depuis le local Storage
+// function displayOrderWithId(orderId) {
+//     const orderIdElement = document.getElementById("orderId")
+//     orderIdElement.textContent = orderId
+// }
+
+// //supprimer les données du cache une fois le form envoyé
+// function clearCache(){
+//     const clear = window.localStorage
+//     clear.clear()
+// }
+
 function getOrderId(){
-const queryString = window.location.search
-const urlParams = new URLSearchParams(queryString)
-return urlParams.get("orderId")
-console.log(orderId)
+    let url = document.location.href
+    url = new URL(url)
+    let id = url.searchParams.get("id")
 
+    document.getElementById("orderId").textContent = id
 }
 
-//envoi les id depuis le local Storage
-function displayOrderWithId(orderId) {
-    const orderIdElement = document.getElementById("orderId")
-    orderIdElement.textContent = orderId
-}
-
-//supprimer les données du cache une fois le form envoyé
-function clearCache(){
-    const clear = window.localStorage
-    clear.clear()
-}
+getOrderId()
